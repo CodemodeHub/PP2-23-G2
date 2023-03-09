0cc46468b8fad95186fc59bebc76d47b811ee469
Ruchanov <98680173+Ruchanov@users.noreply.github.com>
2023-03-09 12:26:04 +0600

    'upd'

--- lesson6/5.py
+++ lesson6/5.py
@@ -1,5 +1,14 @@
 import json
+# json.dump() is used to write the JSON data directly to a file-like object.
+# It takes two arguments: the Python object you want to convert to JSON, and the file-like object you want to write the JSON data to.
 
+# json.dumps() on the other hand, is used to convert a 
+# Python object to a JSON formatted string. 
+
+# json.load() is used to load JSON data from a file-like object. 
+
+# json.loads() on the other hand, is used to load a JSON formatted string into a Python object. 
+# It takes one argument: the JSON formatted string you want to load.
 d = {
     'name' : 'Magzhan',
     'surname': 'Akhmadi',
